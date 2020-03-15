e6b15b9dc2ffabcbcac203b8f7b9455545a94717
BurCha <1477996221@qq.com>
2020-03-15 10:53:19 +0800

    refactor: 统计每天的博客数量

--- src/services/statis.js
+++ src/services/statis.js
@@ -4,6 +4,7 @@
 
 const { User, Blog } = require('../db/model')
 const Sequelize = require('sequelize')
+const seq = require('../db/seq')
 
 //获取用户性别数量
 async function getSexCount(genderType) {
@@ -19,20 +20,9 @@ async function getSexCount(genderType) {
 
 //统计每天的博客数量
 async function getBlogCountOfDay() {
-  const res = await Blog.findAndCountAll({
-    attributes: ['id', 'createdAt'],
-    where: {
-      id: {
-        [Sequelize.Op.gt]: 0,
-      },
-    }
-  })
+  const res = await seq.query("SELECT FROM_UNIXTIME(unix_timestamp(createdAt), '%Y年%m月%d日') as createdAt, count(id) as count FROM blogs where 1 = 1 group by FROM_UNIXTIME(unix_timestamp(createdAt), '%Y年%m月%d')");
 
-  console.log("res", res)
-
-  const blogList = res.rows.map(item => item.dataValues)
-
-  return { blogList }
+  return { blogStatis: res[0] }
 }
 
 module.exports = {
